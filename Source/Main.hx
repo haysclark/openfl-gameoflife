d7ff70f1a2d2ec7a82cfe2bffc9f20d3647d3f2c
Hays Clark <hays@infinitedescent.com>
2013-11-26 23:54:34 -0800

    Adding Basic Contact for Application.  Using a more RobotLegs approach then the MMVC example because of OpenFL.

--- Source/Main.hx
+++ Source/Main.hx
@@ -1,12 +1,23 @@
 package;
 
+import mmvc.api.IViewContainer;
+import gameoflife.GameOfLifeContext;
 import flash.display.Sprite;
 
-class Main extends Sprite
+class Main extends Sprite implements IViewContainer
 {
+    public var viewAdded:Dynamic -> Void;
+    public var viewRemoved:Dynamic -> Void;
+
     public function new()
     {
         super();
-        trace("Game Of Life !");
+
+        var contect = new GameOfLifeContext(this);
+    }
+
+    public function isAdded(view:Dynamic):Bool
+    {
+        return true;
     }
 }
