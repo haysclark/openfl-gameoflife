e78a2fba2167100469429bf048a836bc35b3f08a
Hays Clark <hays@infinitedescent.com>
2013-11-30 18:02:41 -0800

    Adding basic script that build IPA file and sets its iTunes artwork.

---
+++ build_ios_ipa.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+#
+# OpenFL Build to IPA Script iOS 1.0, 2013
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of 
+# this software and associated documentation files (the "Software"), to deal in 
+# the Software without restriction, including without limitation the rights to 
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+# of the Software, and to permit persons to whom the Software is furnished to do 
+# so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all 
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
+# SOFTWARE.
+# 
+# Written by Hays Clark <hays@infinitedescent.com>
+# 
+# Latest version can be found at http://www.nixcraft.com/
+# Tested on OSX (10.9)
+# 
+# Copyright (c) 2013 Hays Clark
+# 
+
+EXPORT_APP_PATH="Export/ios/build/Release-iphoneos/"
+if [ ! -d "$EXPORT_APP_PATH" ]; then
+	echo "Can not find iOS project. Try 'openfl build ios'"
+	exit 1;
+fi
+
+OPEN_DIR=0
+HAD_ARGS=0
+APP_NAME=""
+
+while getopts "ao:" VALUE "$@" ; do
+	if [ "$VALUE" = "a" ] ; then
+		OPEN_DIR=1
+	fi
+	if [ "$VALUE" = "o" ] ; then
+		APP_NAME="$OPTARG"
+	fi
+	if [ "$VALUE" = ":" ] ; then
+        echo "Flag -$OPTARG requires an argument."
+        echo "Usage: $0 [-a] [-o outputfile]"
+        exit 1
+    fi
+	if [ "$VALUE" = "?" ] ; then
+		echo "Unknown flag -$OPTARG detected."
+		echo "Usage: $0 [-a] [-o outputfile]"
+		exit 1
+	fi
+done
+
+if [ "$APP_NAME" = "" ]; then
+	#is there a better way?
+	APP_FILE=$(find $EXPORT_APP_PATH -type f | grep -o '.*app' | head -1)
+	APP_BASENAME=$(basename $APP_FILE)
+	APP_NAME=${APP_BASENAME%.*}
+fi
+
+IPA_PATH=$APP_NAME.ipa
+echo "Building "$IPA_PATH
+
+SEARCH=$EXPORT_APP_PATH*.app
+
+if [ -d "Payload" ]; then
+	echo " - removing existing export path: Payload"
+	rm -rf Payload
+fi
+
+mkdir Payload
+APP_DIR=Payload/$APP_BASENAME/
+
+cp -rp $SEARCH Payload/
+cp -a ios/. $APP_DIR
+
+if [ -f $APP_DIR"iTunesArtwork.png" ]; then
+	echo " - removing .png suffix from iTunesArtwork"
+	mv $APP_DIR/iTunesArtwork.png $APP_DIR/iTunesArtwork
+fi
+
+zip -r --quiet $IPA_PATH Payload
+rm -rf Payload
+
+if [ $OPEN_DIR = 1 ]; then
+	open .
+fi
